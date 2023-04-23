3616fc0db08a0be6b9fac614f0dfaee4ecfb7454
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-04-23 14:59:59 -0400

    index.js: Added the express server start boilerplate code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Start your es6 scripts here
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
