f3ef259e06321c5599ad66845037aa6833e186de
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-04-23 15:46:22 -0400

    index.js: Update the index.js to run the graphQL route with the graphiql web interface

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express');
+import express from ('express');
+import resolvers from "./resolvers";
+import schema from "./schema";
+
+import { graphqlHTTP } from "express-graphql";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: resolvers,
+  graphiql: true
+}));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
