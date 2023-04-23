577aeefef6c6f03694d94e3da27ad5e897e9ee44
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-04-23 15:05:53 -0400

    schema.js: Added the type course schema

--- schema.js
+++ schema.js
@@ -1,7 +1,16 @@
 import { buildSchema } from "graphql";
 
 const schema = buildSchema(`
-
+    type Course {
+        id: ID
+        courseName: String
+        category: String
+        price: Int
+        language: String
+        email: String
+        stack: String
+        teachingAssists: [teachingAssists]
+    }
 `);
 
 export default schema;
